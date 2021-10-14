29443dbf5b58f3d95f249fab47a1aafcbf96cf7b
chakrireddy1609 <chakrireddy1609@gmail.com>
2021-10-14 17:34:11 +0530

    Share Calculator V2

--- Tests/BaseTest.py
+++ Tests/BaseTest.py
@@ -1,3 +1,6 @@
+import inspect
+import logging
+
 import pytest
 from selenium.webdriver import Keys
 
@@ -7,7 +10,17 @@ from Utilities.TestData import TestData
 
 @pytest.mark.usefixtures("setup")
 class Test_Base:
-    pass
+    def getLogger(self):
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
+        fileHandler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
+
+        logger.addHandler(fileHandler)  # filehandler object
+
+        logger.setLevel(logging.DEBUG)
+        return logger
 
 
 
